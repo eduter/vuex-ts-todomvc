97f2af41c94ecfb5c7831d9e63852935382797e6
Eduardo Terroso <eduter@fareoffice.com>
2019-02-19 17:09:59 +0100

    Copied todomvc from Vuex examples

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
+import "babel-polyfill";
 import Vue from "vue";
-import App from "./App.vue";
-import store from "./store";
+import App from "./components/App.vue";
+// @ts-ignore
+import store from "./store/index";
 
 Vue.config.productionTip = false;
 
-new Vue({
-  store,
-  render: h => h(App)
-}).$mount("#app");
+// @ts-ignore
+new Vue({ store, el: "#app", render: h => h(App) });
